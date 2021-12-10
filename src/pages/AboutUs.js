b3fb45f59f3f1d50cb819b20a668df6e928e6c35
qretsar <qretsar@gmail.com>
2021-12-10 18:16:31 +0100

    Services Section added

---
+++ src/pages/AboutUs.js
@@ -0,0 +1,14 @@
+import React from "react";
+//Page Components
+import AboutSection from "../components/AboutSection";
+import ServicesSection from "../components/ServicesSection";
+const AboutUS = () => {
+  return (
+    <>
+      <AboutSection />
+      <ServicesSection />
+    </>
+  );
+};
+
+export default AboutUS;
